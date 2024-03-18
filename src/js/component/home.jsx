e623c5ab2cace5799ec1a53108cb6e593e0b793b
ayaalakwaa <153412822+ayaalakwaa@users.noreply.github.com>
2024-03-18 21:05:00 +0000

    todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  const [count, setCount] = useState(0);
+  const [showDelete, setShowDelete] = useState(-1);
+  const deleteItem = (recived) => {
+    const newList = list.filter(
+      (_filterTask, filterIndex) => recived != filterIndex
+    );
+    setList(newList);
+  };
+  useEffect(() => {
+    setCount(list.length);
+  }, [list]);
+  const changeTask = count < 2 ? "task" : "tasks";
+  return (
+    <div className="text-center">
+      <h1>todos</h1>
+      <div className="inputTasks">
+        <input
+          type="text"
+          onKeyDown={(event) => {
+            if (event.key == 'Enter') {
+              setList([...list, input]);
+              setInput("");
+            }
+          }}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder={
+            list.length === 0
+              ? "what needs to be done?"
+              : "what needs to be done?"
+          }
+        ></input>
+      </div>
+      {list.length > 0
+        ? list.map((mapTask, mapIndex) => {
+            return (
+              <div
+                className="rowTask effect"
+                onMouseEnter={() => setShowDelete(mapIndex)}
+                onMouseLeave={() => setShowDelete(-1)}
+              >
+                <div>
+                  <ul>
+                    <li>{mapTask}</li>
+                  </ul>
+                </div>
+                <div
+                  onClick={() => deleteItem(mapIndex)}
+                  className={`${
+                    mapIndex == showDelete ? "d-block" : "d-none"
+                  } iconDelete`}
+                  onMouseEnter={(e) => {
+                    e.currentTarget.style.color = "red";
+                  }}
+                  onMouseLeave={(e) => {
+                    e.currentTarget.style.color = "";
+                  }}
+                >
+                  <i
+                    class="fas fa-trash-alt"
+                    title="Delete"
+                    style={{ cursor: "pointer" }}
+                  ></i>
+                </div>
+              </div>
+            );
+          })
+        : ""}
+      <div className="countTask">
+        {count} {changeTask}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
